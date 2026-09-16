07e928ccb38f4eb78dbc5c653ca8229e0d3e55a5
ZENCURSE <manibharathi21062@gmail.com>
2026-09-16 15:30:55 +0530

    Update programmer and developer contact information

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,12 +2,12 @@ __name__ = "Rename-Bot"
 __version__ = "3.1.0"
 __license__ = " Apache License, Version 2.0"
 __copyright__ = ""
-__programer__ = "<a href=https://t.me/Unrated_Coder>Unrated Coder</a>"
+__programer__ = "<a href=https://t.me/CodeRips>CodeRips</a>"
 __library__ = "<a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>"
 __language__ = "<a href=https://www.python.org/>Pyᴛʜᴏɴ 3</a>"
 __database__ = "<a href=https://cloud.mongodb.com/>Mᴏɴɢᴏ DB</a>"
-__developer__ = "<a href=https://t.me/Unrated_Coder>Unrated Coder</a>"
-__maindeveloper__ = "<a href=https://t.me/Unrated_Coder>Unrated Coder</a>"
+__developer__ = "<a href=https://t.me/ZENCURSE>ZENCURSE</a>"
+__maindeveloper__ = "<a href=https://t.me/CodeRips>CodeRips</a>"
 
 from plugins.force_sub import not_subscribed, forces_sub, handle_banned_user_status
 from pyrogram import Client, filters
